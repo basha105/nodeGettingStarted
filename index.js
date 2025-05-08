ffb6809af864b4b27bf962e5e0f814199756c870
basha105 <brentbasha17@gmail.com>
2025-05-08 10:38:05 -0700

    Fix

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const url = require('url');
 const fs = require('fs');
 
 http.createServer(function (req, res) {
-    var parsedFile = url.parse(req.url, true);
-    var filename = '.' + parsedFile.pathname;
-    fs.readFile(filename, function(err, data) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write(404.html);
-    });
+    var parsedUrl = url.parse(req.url, true);
+    var filename = '.' + parsedUrl.pathname;
 
+    if (filename === '.') {
+        readFile('./index.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+        });
+    }
+    else if (filename === './about' || filename === './contact-me') {
+        readFile(filename+'.html', function(err, data) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+        });
+    }
+    else {
+        readFile('./404.html', function(err, data) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.write(data);
+        });
+    }
+
+    
 }).listen(8080);
